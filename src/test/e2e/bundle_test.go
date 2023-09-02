d7bf56ad522a9df4e803c5e6964551e08bd008fc
Anthony <anthony@defenseunicorns.com>
2023-09-02 16:29:31 -0600

    Cleanup Local Init Pkg test

--- src/test/e2e/bundle_test.go
+++ src/test/e2e/bundle_test.go
@@ -90,13 +90,6 @@ func TestBundleWithLocalInitPkg(t *testing.T) {
 	e2e.SetupWithCluster(t)
 
 	e2e.DownloadZarfInitPkg(t, zarfVersion)
-	e2e.CreateZarfPkg(t, "src/test/packages/zarf/podinfo")
-
-	e2e.SetupDockerRegistry(t, 888)
-	defer e2e.TeardownRegistry(t, 888)
-
-	pkg := fmt.Sprintf("src/test/packages/zarf/zarf-init-%s-%s.tar.zst", e2e.Arch, zarfVersion)
-	zarfPublish(t, pkg, "localhost:888")
 
 	bundleDir := "src/test/packages/04-local-init"
 	bundlePath := filepath.Join(bundleDir, fmt.Sprintf("uds-bundle-local-init-%s-0.0.1.tar.zst", e2e.Arch))
