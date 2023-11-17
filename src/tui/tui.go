ebcf5ccc8a377355c753472aa1a9cc1cac93344f
unclegedd <gedd@defenseunicorns.com>
2023-11-17 15:18:34 -0600

    WIP: stuff is appearing...but its ugly

--- src/tui/tui.go
+++ src/tui/tui.go
@@ -4,15 +4,12 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
-	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/defenseunicorns/zarf/src/pkg/message"
-	"github.com/pterm/pterm"
 
 	"github.com/defenseunicorns/uds-cli/src/pkg/bundle"
 )
@@ -24,47 +21,25 @@ const (
 	tick     operation = "tick"
 )
 
-// You generally won't need this unless you're processing stuff with
-// complicated ANSI escape sequences. Turn it on if you notice flickering.
-//
-// Also keep in mind that high performance rendering only works for programs
-// that use the full size of the terminal. We're enabling that below with
-// tea.EnterAltScreen().
-const useHighPerformanceRenderer = false
-
-var (
-	titleStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Right = "├"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 1)
-	}()
-
-	infoStyle = func() lipgloss.Style {
-		b := lipgloss.RoundedBorder()
-		b.Left = "┤"
-		return titleStyle.Copy().BorderStyle(b)
-	}()
-)
-
 func InitModel(content string, client *bundle.Bundler, buf *bytes.Buffer, op operation) Model {
 	return Model{
 		content:             content,
-		viewport:            viewport.Model{},
 		bndlClient:          client,
 		packageOutputBuffer: buf,
 		op:                  op,
 		quitChan:            make(chan int),
+		progress:            progress.New(progress.WithDefaultGradient()),
 	}
 }
 
 type Model struct {
 	content             string
 	ready               bool
-	viewport            viewport.Model
 	packageOutputBuffer *bytes.Buffer
 	bndlClient          *bundle.Bundler
 	op                  operation
 	quitChan            chan int
+	progress            progress.Model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -91,7 +66,7 @@ func cleanFlushInfo(bytesBuffer *bytes.Buffer) string {
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var (
-		cmd  tea.Cmd
+		//cmd  tea.Cmd
 		cmds []tea.Cmd
 	)
 	select {
@@ -102,27 +77,14 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case operation:
 			// todo: this should probably go somewhere else
 			m.content += cleanFlushInfo(m.packageOutputBuffer)
-			m.viewport.SetContent(m.content)
+			//m.viewport.SetContent(m.content)
 			m.ready = true
 			m.packageOutputBuffer.Reset()
-			// autoscroll the contents of the viewport
-			//m.viewport.GotoBottom()
 
 			switch msg {
 			case DeployOp:
 				// run Deploy concurrently so we can update the TUI while it runs
-				go func() {
-					// todo: don't actually put the buffer in the call to Deploy()
-					if err := m.bndlClient.Deploy(m.packageOutputBuffer); err != nil {
-						// use existing Zarf pterm things for errors
-						pterm.EnableOutput()
-						pterm.SetDefaultOutput(os.Stderr)
-						m.bndlClient.ClearPaths()
-						message.Fatalf(err, "Failed to deploy bundle: %s", err.Error())
-						m.quitChan <- 1
-					}
-					//m.quitChan <- 1
-				}()
+
 				// use a ticker to update the TUI while the deploy runs
 				return m, tea.Tick(time.Millisecond, func(time.Time) tea.Msg {
 					return tick
@@ -137,47 +99,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			if k := msg.String(); k == "ctrl+c" || k == "q" || k == "esc" {
 				return m, tea.Quit
 			}
-
-		case tea.WindowSizeMsg:
-			headerHeight := lipgloss.Height(m.headerView())
-			footerHeight := lipgloss.Height(m.footerView())
-			verticalMarginHeight := headerHeight + footerHeight
-
-			if !m.ready {
-				// Since this program is using the full size of the viewport we
-				// need to wait until we've received the window dimensions before
-				// we can initialize the viewport. The initial dimensions come in
-				// quickly, though asynchronously, which is why we wait for them
-				// here.
-				m.viewport = viewport.New(msg.Width, msg.Height-verticalMarginHeight)
-				m.viewport.YPosition = headerHeight
-				m.viewport.HighPerformanceRendering = useHighPerformanceRenderer
-				m.viewport.SetContent(m.content)
-				m.ready = true
-
-				// This is only necessary for high performance rendering, which in
-				// most cases you won't need.
-				//
-				// Render the viewport one line below the header.
-				m.viewport.YPosition = headerHeight + 1
-			} else {
-				m.viewport.Width = msg.Width
-				m.viewport.Height = msg.Height - verticalMarginHeight
-			}
-
-			if useHighPerformanceRenderer {
-				// Render (or re-render) the whole viewport. Necessary both to
-				// initialize the viewport and when the window is resized.
-				//
-				// This is needed for high-performance rendering only.
-				cmds = append(cmds, viewport.Sync(m.viewport))
-			}
 		}
 	}
-
-	// Handle keyboard and mouse events in the viewport
-	m.viewport, cmd = m.viewport.Update(msg)
-	cmds = append(cmds, cmd)
 
 	return m, tea.Batch(cmds...)
 }
@@ -186,19 +109,27 @@ func (m Model) View() string {
 	if !m.ready {
 		return "\n  Initializing..."
 	}
-	return fmt.Sprintf("%s\n%s\n%s", m.headerView(), m.viewport.View(), m.footerView())
+	return fmt.Sprintf("%s", m.boxView())
 }
 
-func (m Model) headerView() string {
-	title := titleStyle.Render("📦 Deploying: podinfo")
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(title)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
+func textInBox(text string) string {
+	return lipgloss.NewStyle().MarginLeft(1).Width(100).Background(lipgloss.Color("#E81E27")).Render(text)
 }
 
-func (m Model) footerView() string {
-	info := infoStyle.Render(fmt.Sprintf("%3.f%%", m.viewport.ScrollPercent()*100))
-	line := strings.Repeat("─", max(0, m.viewport.Width-lipgloss.Width(info)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, line, info)
+func (m Model) progressBar() string {
+	m.progress.EmptyColor = "#98e695"
+	return lipgloss.NewStyle().Render(m.progress.View())
+
+}
+
+func (m Model) boxView() string {
+	boxStyle := func() lipgloss.Style {
+		b := lipgloss.RoundedBorder()
+		return lipgloss.NewStyle().BorderStyle(b).Width(100).BorderForeground(lipgloss.Color("#874BFD"))
+	}()
+
+	box := boxStyle.Render(lipgloss.JoinHorizontal(0.2, textInBox("📦 Deploying: podinfo"), m.progressBar()))
+	return box
 }
 
 func max(a, b int) int {
