8f7cc7a783b842ed1f2790f9d503e6fb47103abd
Micah Nagel <micah.nagel@defenseunicorns.com>
2024-02-22 12:06:04 -0700

    wip: find images

--- src/pkg/generate/generate.go
+++ src/pkg/generate/generate.go
@@ -1,25 +1,56 @@
 package generate
 
 import (
+	"fmt"
+
 	"github.com/defenseunicorns/uds-cli/src/config"
+	"github.com/defenseunicorns/zarf/src/cmd/common"
+	"github.com/defenseunicorns/zarf/src/pkg/packager"
 	"github.com/defenseunicorns/zarf/src/pkg/utils"
 	"github.com/defenseunicorns/zarf/src/types"
 )
 
 func Generate() {
+	// Generate the metadata
 	metadata := types.ZarfMetadata{
 		Name:    config.GenerateChartName,
 		Version: config.GenerateChartVersion + "-uds.0",
 		URL:     config.GenerateChartUrl,
-		Authors: "Rob Ferguson",
+		Authors: "2 days no pROBlem",
 	}
 
-	// component :=
+	// Generate the chart
+	chart := types.ZarfChart{
+		Name:      config.GenerateChartName,
+		Namespace: config.GenerateChartName,
+		URL:       config.GenerateChartUrl,
+		Version:   config.GenerateChartVersion,
+	}
 
+	// Generate the component
+	component := types.ZarfComponent{
+		Name:     config.GenerateChartName,
+		Required: true,
+		Charts:   []types.ZarfChart{chart},
+	}
+	components := []types.ZarfComponent{component}
+
+	// Generate the package
 	packageInstance := types.ZarfPackage{
 		Kind:       types.ZarfPackageConfig,
 		Metadata:   metadata,
-		Components: []types.ZarfComponent{},
+		Components: components,
 	}
+
+	packagerConfig := types.PackagerConfig{
+		Pkg: packageInstance,
+	}
+	common.SetBaseDirectory(nil, &packagerConfig)
+
+	packager := packager.NewOrDie(&packagerConfig)
+	defer packager.ClearTempPaths()
+	images, _ := packager.FindImages()
+	fmt.Println(images)
+
 	utils.ColorPrintYAML(packageInstance, nil, false)
 }
