5ffe8d93d92e564e10545693f120eefe5f1da6a4
Micah Nagel <micah.nagel@defenseunicorns.com>
2024-02-22 14:17:59 -0700

    wip: fixes

--- src/pkg/generate/generate.go
+++ src/pkg/generate/generate.go
@@ -1,7 +1,11 @@
 package generate
 
 import (
+	"embed"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/defenseunicorns/uds-cli/src/config"
 	"github.com/defenseunicorns/zarf/src/cmd/common"
@@ -11,6 +15,10 @@ import (
 	goyaml "github.com/goccy/go-yaml"
 )
 
+//go:embed chart/*
+var folder embed.FS
+
+// Generate a UDS Package from a given helm chart in the config
 func Generate() {
 	// Generate the metadata
 	metadata := types.ZarfMetadata{
@@ -21,12 +29,12 @@ func Generate() {
 	}
 
 	// Generate the config chart zarf yaml
-	// configChart := types.ZarfChart{
-	// 	Name:      "uds-config",
-	// 	Namespace: config.GenerateChartName,
-	// 	LocalPath: "chart",
-	// 	Version:   "0.0.1",
-	// }
+	configChart := types.ZarfChart{
+		Name:      "uds-config",
+		Namespace: config.GenerateChartName,
+		LocalPath: "chart",
+		Version:   "0.1.0",
+	}
 
 	// Generate the upstream chart zarf yaml
 	upstreamChart := types.ZarfChart{
@@ -40,7 +48,7 @@ func Generate() {
 	component := types.ZarfComponent{
 		Name:     config.GenerateChartName,
 		Required: true,
-		Charts:   []types.ZarfChart{upstreamChart},
+		Charts:   []types.ZarfChart{configChart, upstreamChart},
 		Only: types.ZarfComponentOnlyTarget{
 			Flavor: "upstream",
 		},
@@ -57,6 +65,9 @@ func Generate() {
 	// Write in progress zarf yaml to a file
 	text, _ := goyaml.Marshal(packageInstance)
 	os.WriteFile("zarf.yaml", text, 0644)
+
+	// Copy template chart to destination
+	writeChart(folder)
 
 	// Find images to add to the component
 	packagerConfig := types.PackagerConfig{
@@ -81,11 +92,61 @@ func Generate() {
 	// TODO: Strip off cosign signatures/attestations?
 	components[0].Images = images[config.GenerateChartName]
 
-	// Generate UDS Config chart
-
 	utils.ColorPrintYAML(packageInstance, nil, false)
 
-	// Write in progress zarf yaml to a file
+	// Write final zarf yaml to a file
 	text, _ = goyaml.Marshal(packageInstance)
 	os.WriteFile("zarf.yaml", text, 0644)
+}
+
+// Write an embedded folder (template helm chart) to the localDir
+func writeChart(folder embed.FS) {
+	// Destination directory where you want to write the files
+	destDir := "."
+
+	// Create destination directory if it doesn't exist
+	if err := os.MkdirAll(destDir, 0755); err != nil {
+		panic(err)
+	}
+
+	// Walk through the embedded filesystem
+	err := fs.WalkDir(folder, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Construct destination file path
+		destPath := filepath.Join(destDir, path)
+
+		if d.IsDir() {
+			// Create directory if it doesn't exist
+			return os.MkdirAll(destPath, 0755)
+		}
+
+		// Open source file from embedded filesystem
+		srcFile, err := folder.Open(path)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+
+		// Create destination file
+		destFile, err := os.Create(destPath)
+		if err != nil {
+			return err
+		}
+		defer destFile.Close()
+
+		// Copy contents from source to destination
+		_, err = io.Copy(destFile, srcFile)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
 }
