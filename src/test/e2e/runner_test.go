87b826e99ba8b87fc2f567f00ac93e81b8038954
Darcy Cleaver <darcy@defenseunicorns.com>
2024-03-13 16:50:26 -0600

    make env sensitive tests non parallel

--- src/test/e2e/runner_test.go
+++ src/test/e2e/runner_test.go
@@ -323,7 +323,6 @@ func TestTaskRunner(t *testing.T) {
 	})
 
 	t.Run("test task to load env vars using the envPath key", func(t *testing.T) {
-		t.Parallel()
 		stdOut, stdErr, err := e2e.UDS("run", "env-from-file", "--file", "src/test/tasks/tasks.yaml")
 		require.NoError(t, err, stdOut, stdErr)
 		require.Contains(t, stdErr, e2e.Arch)
@@ -350,17 +349,18 @@ func TestTaskRunner(t *testing.T) {
 		require.Contains(t, stdErr, "env-var")
 	})
 	t.Run("test that ARCHITECTURE env var is getting passed to runner", func(t *testing.T) {
-		t.Parallel()
 		os.Setenv("UDS_ARCHITECTURE", "amd64")
 		stdOut, stdErr, err := e2e.UDS("run", "echo-architecture", "--file", "src/test/tasks/tasks.yaml")
 		require.NoError(t, err, stdOut, stdErr)
-		require.NotContains(t, stdErr, "default")
+		require.NotContains(t, stdErr, "arm64")
 		require.Contains(t, stdErr, "amd64")
 
 		os.Setenv("UDS_ARCHITECTURE", "arm64")
 		stdOut, stdErr, err = e2e.UDS("run", "echo-architecture", "--file", "src/test/tasks/tasks.yaml")
 		require.NoError(t, err, stdOut, stdErr)
-		require.NotContains(t, stdErr, "default")
+		require.NotContains(t, stdErr, "amd64")
 		require.Contains(t, stdErr, "arm64")
+
+		os.Setenv("UDS_ARCHITECTURE", "")
 	})
 }
