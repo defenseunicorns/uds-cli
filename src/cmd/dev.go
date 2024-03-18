e5368269819d8a9681420fd4692d737c468cc4e2
Darcy Cleaver <darcy@defenseunicorns.com>
2024-03-18 13:47:49 -0600

    wip

---
+++ src/cmd/dev.go
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The UDS Authors
+
+// Package cmd contains the CLI commands for UDS.
+package cmd
+
+// "os"
+// "path/filepath"
+// "strings"
+
+// "github.com/AlecAivazis/survey/v2"
+// "github.com/defenseunicorns/uds-cli/src/config"
+// "github.com/defenseunicorns/uds-cli/src/config/lang"
+// "github.com/defenseunicorns/uds-cli/src/pkg/bundle"
+// zarfConfig "github.com/defenseunicorns/zarf/src/config"
+// "github.com/defenseunicorns/zarf/src/pkg/message"
+// zarfUtils "github.com/defenseunicorns/zarf/src/pkg/utils"
+// zarfTypes "github.com/defenseunicorns/zarf/src/types"
+// goyaml "github.com/goccy/go-yaml"
+// "github.com/spf13/cobra"
+
+// var devCmd = &cobra.Command{
+// 	Use:   "dev",
+// 	Short: lang.CmdDevShort,
+// }
+
+// var devDeployCmd = &cobra.Command{
+// 	Use:   "deploy",
+// 	Args:  cobra.MaximumNArgs(1),
+// 	Short: lang.CmdDevDeployShort,
+// 	PreRun: func(_ *cobra.Command, args []string) {
+// 		CreatePreRun(args)
+// 	},
+// 	Run: func(_ *cobra.Command, args []string) {
+// 	},
+// }
