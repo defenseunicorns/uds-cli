505b49b91c7f92e426c19d8bfd932e56364330a0
Darcy Cleaver <darcy@defenseunicorns.com>
2024-03-28 16:00:06 -0600

    fix zarf package create

---
+++ src/pkg/bundle/dev.go
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present The UDS Authors
+
+// Package bundle contains functions for interacting with, managing and deploying UDS packages
+package bundle
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/defenseunicorns/uds-cli/src/config"
+	zarfCLI "github.com/defenseunicorns/zarf/src/cmd"
+	"github.com/defenseunicorns/zarf/src/pkg/message"
+	zarfUtils "github.com/defenseunicorns/zarf/src/pkg/utils"
+)
+
+// CreateZarfPkg creates a zarf package if its missing when in dev mode
+func (b *Bundle) CreateZarfPkgs(srcDir string) {
+	path := filepath.Join(srcDir, b.cfg.CreateOpts.BundleFile)
+	if err := zarfUtils.ReadYaml(path, &b.bundle); err != nil {
+		message.Fatalf(err, "Failed to read bundle.yaml: %s", err.Error())
+	}
+
+	zarfPackagePattern := `^zarf-.*\.tar\.zst$`
+	for _, pkg := range b.bundle.Packages {
+		if pkg.Repository == "" {
+			path := srcDir + pkg.Path
+			// get files in directory
+			files, err := os.ReadDir(path)
+			if err != nil {
+				message.Fatalf(err, "Failed to obtain package %s: %s", pkg.Name, err.Error())
+			}
+			regex := regexp.MustCompile(zarfPackagePattern)
+
+			// check if package exists
+			packageFound := false
+			for _, file := range files {
+				if regex.MatchString(file.Name()) {
+					packageFound = true
+					break
+				}
+			}
+			// create local zarf package if it doesn't exist
+			if !packageFound {
+				os.Args = []string{"zarf", "package", "create", path, "--confirm", "-o", path}
+				zarfCLI.Execute()
+				if err != nil {
+					message.Fatalf(err, "Failed to create package %s: %s", pkg.Name, err.Error())
+				}
+			}
+		}
+	}
+}
+
+// setDevSource sets the source for the bundle when in dev mode
+func (b *Bundle) SetDevSource(srcDir string) {
+	srcDir = filepath.Dir(srcDir)
+	// Add a trailing slash if it's missing
+	if len(srcDir) != 0 && srcDir[len(srcDir)-1] != '/' {
+		srcDir = srcDir + "/"
+	}
+	filename := fmt.Sprintf("%s%s-%s-%s.tar.zst", config.DevBundlePrefix, b.bundle.Metadata.Name, b.bundle.Metadata.Architecture, b.bundle.Metadata.Version)
+	b.cfg.DeployOpts.Source = filepath.Join(srcDir, filename)
+}
