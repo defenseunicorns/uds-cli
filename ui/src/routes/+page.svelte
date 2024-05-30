34c6f9a617e42ddab624533642082937b926d6a4
Billy Figueroa <billy@defenseunicorns.com>
2024-05-30 14:24:34 -0400

    Simplifying header navigation

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -22,7 +22,7 @@
   <meta name="description" content="Home -- Search for repos" />
 </svelte:head>
 
-<div>
+<div class="welcome__container">
   <h2 class="header">Welcome to UDS</h2>
 
   <p class="subheader">Find the applications running in your cluster and accessible to you here.</p>
@@ -148,24 +148,27 @@
 </div>
 
 <style lang="scss">
+  .welcome__container {
+    display: grid;
+    grid-gap: 17px;
+  }
+
   .header {
     font-size: 16px;
     font-weight: 600;
-    line-height: 32px;
+    // line-height: 32px;
   }
 
   .subheader {
+    color: var(--cds-text-secondary);
     font-size: var(--cds-helper-text-01-font-size);
-    line-height: 16px;
+    line-height: var(--cds-helper-text-01-font-size);
     letter-spacing: 0.32px;
   }
 
   .cards {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: var(--cds-spacing-04);
-    grid-row-gap: var(--cds-spacing-06);
-
-    margin-top: var(--cds-spacing-07);
+    grid-gap: var(--cds-spacing-05);
   }
 </style>
