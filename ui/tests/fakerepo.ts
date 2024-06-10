dc4910cf09e0e8e52248b7321fabdbaaa36c6414
unclegedd <gedd@defenseunicorns.com>
2024-06-10 13:18:58 -0500

    running format

--- ui/tests/fakerepo.ts
+++ ui/tests/fakerepo.ts
@@ -1,27 +1,27 @@
-import type{Pod} from 'kubernetes-types/core/v1'
-import type {PodRepository} from "$lib/repos/repository";
+import type { Pod } from 'kubernetes-types/core/v1';
+import type { PodRepository } from '$lib/repos/repository';
 
 export const testData: Pod[] = [
-    {
-        apiVersion: "v1",
-        kind: "Pod",
-        metadata: {
-            name: "pod1",
-            namespace: "default"
-        }
-    },
-    {
-        apiVersion: "v1",
-        kind: "Pod",
-        metadata: {
-            name: "pod2",
-            namespace: "default"
-        }
-    },
-]
-
-export class FakePodRepository implements PodRepository{
-    getPods(): Promise<Pod[]> {
-        return Promise.resolve(testData)
+  {
+    apiVersion: 'v1',
+    kind: 'Pod',
+    metadata: {
+      name: 'pod1',
+      namespace: 'default'
     }
+  },
+  {
+    apiVersion: 'v1',
+    kind: 'Pod',
+    metadata: {
+      name: 'pod2',
+      namespace: 'default'
+    }
+  }
+];
+
+export class FakePodRepository implements PodRepository {
+  getPods(): Promise<Pod[]> {
+    return Promise.resolve(testData);
+  }
 }
