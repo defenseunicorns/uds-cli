54df1fe393a266ae9a5aff277879674c5547d4e2
Billy Figueroa <figuer25@gmail.com>
2024-06-12 11:00:02 -0400

    Refactoring the side nav (#684)

---
+++ ui/src/lib/components/SideNav/SideNavMenu.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<li class="bx--side-nav__item">
+  <div aria-expanded="true" class="bx--side-nav__submenu">
+    <span class="bx--side-nav__submenu-title">{text}</span>
+  </div>
+
+  {#if $$slots.default}
+    <ul role="menu" class="bx--side-nav__menu">
+      <slot />
+    </ul>
+  {/if}
+</li>
