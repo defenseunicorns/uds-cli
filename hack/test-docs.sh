97f5fbfd1b031ff747f68d012c995053c1a647b1
UncleGedd <42304551+UncleGedd@users.noreply.github.com>
2024-07-09 13:12:30 -0500

    Update hack/test-docs.sh
    Co-authored-by: Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>

--- hack/test-docs.sh
+++ hack/test-docs.sh
@@ -4,7 +4,7 @@ check_git_status() {
     if [ -z "$(git status -s "$1")" ]; then
         echo "Success!"
     else
-        echo "Schema changes found, please regenerate $1"
+        echo "Docs changes found, please regenerate $1"
         git status "$1"
         exit 1
     fi
