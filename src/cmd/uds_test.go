93587140f5cb765b4887cfd1875871f0c04550c6
Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>
2024-07-12 08:47:20 -0600

    chore: more overrides refactors (#761)

--- src/cmd/uds_test.go
+++ src/cmd/uds_test.go
@@ -12,44 +12,38 @@ import (
 )
 
 func TestUnmarshalAndValidateConfig(t *testing.T) {
-	type args struct {
-		configFile []byte
-		bundleCfg  *types.BundleConfig
-	}
 	tests := []struct {
 		name        string
-		args        args
+		configFile  []byte
+		bundleCfg   *types.BundleConfig
 		wantErr     bool
 		errContains string
 	}{
 		{
 			name: "Invalid option key",
-			args: args{
-				configFile: []byte(`
+			configFile: []byte(`
 options:
   log_levelx: debug
 `),
-				bundleCfg: &types.BundleConfig{},
-			},
+			bundleCfg: &types.BundleConfig{},
+
 			wantErr:     true,
 			errContains: "invalid config option: log_levelx",
 		},
 		{
 			name: "Option typo",
-			args: args{
-				configFile: []byte(`
+			configFile: []byte(`
 optionx:
   log_level: debug
 `),
-				bundleCfg: &types.BundleConfig{},
-			},
-			wantErr: true,
+			bundleCfg: &types.BundleConfig{},
+			wantErr:   true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := unmarshalAndValidateConfig(tt.args.configFile, tt.args.bundleCfg)
+			err := unmarshalAndValidateConfig(tt.configFile, tt.bundleCfg)
 			if tt.wantErr {
 				require.NotNil(t, err, "Expected error")
 				require.Contains(t, err.Error(), tt.errContains, "Error message should contain the expected string")
--- src/pkg/bundle/common_test.go
+++ src/pkg/bundle/common_test.go
@@ -13,55 +13,44 @@ import (
 )
 
 func Test_validateBundleVars(t *testing.T) {
-	type args struct {
-		packages []types.Package
-	}
 	tests := []struct {
 		name        string
 		description string
-		args        args
+		packages    []types.Package
 		wantErr     bool
 	}{
 		{
 			name:        "ImportMatchesExport",
 			description: "import matches export",
-			args: args{
-				packages: []types.Package{
-					{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
-					{Name: "bar", Imports: []types.BundleVariableImport{{Name: "foo", Package: "foo"}}},
-				},
+			packages: []types.Package{
+				{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
+				{Name: "bar", Imports: []types.BundleVariableImport{{Name: "foo", Package: "foo"}}},
 			},
 			wantErr: false,
 		},
 		{
 			name:        "ImportDoesntMatchExport",
 			description: "error when import doesn't match export",
-			args: args{
-				packages: []types.Package{
-					{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
-					{Name: "bar", Imports: []types.BundleVariableImport{{Name: "bar", Package: "foo"}}},
-				},
+			packages: []types.Package{
+				{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
+				{Name: "bar", Imports: []types.BundleVariableImport{{Name: "bar", Package: "foo"}}},
 			},
 			wantErr: true,
 		},
 		{
 			name:        "FirstPkgHasImport",
 			description: "error when first pkg has an import",
-			args: args{
-				packages: []types.Package{
-					{Name: "foo", Imports: []types.BundleVariableImport{{Name: "foo", Package: "foo"}}},
-				},
+			packages: []types.Package{
+				{Name: "foo", Imports: []types.BundleVariableImport{{Name: "foo", Package: "foo"}}},
 			},
 			wantErr: true,
 		},
 		{
 			name:        "PackageNamesMustMatch",
 			description: "error when package name doesn't match",
-			args: args{
-				packages: []types.Package{
-					{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
-					{Name: "bar", Imports: []types.BundleVariableImport{{Name: "foo", Package: "baz"}}},
-				},
+			packages: []types.Package{
+				{Name: "foo", Exports: []types.BundleVariableExport{{Name: "foo"}}},
+				{Name: "bar", Imports: []types.BundleVariableImport{{Name: "foo", Package: "baz"}}},
 			},
 			wantErr: true,
 		},
@@ -69,7 +58,7 @@ func Test_validateBundleVars(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := validateBundleVars(tt.args.packages)
+			err := validateBundleVars(tt.packages)
 			if tt.wantErr {
 				require.Error(t, err)
 				return
@@ -80,74 +69,67 @@ func Test_validateBundleVars(t *testing.T) {
 }
 
 func Test_validateOverrides(t *testing.T) {
-	type args struct {
+	tests := []struct {
+		name          string
+		description   string
 		bundlePackage types.Package
 		zarfPackage   zarfTypes.ZarfPackage
-	}
-	tests := []struct {
-		name        string
-		description string
-		args        args
-		wantErr     bool
+		wantErr       bool
 	}{
 		{
 			name:        "validOverride",
 			description: "Respective components and charts exist for override",
-			args: args{
-				bundlePackage: types.Package{
-					Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component": {"chart": {}}}},
-				zarfPackage: zarfTypes.ZarfPackage{
-					Components: []zarfTypes.ZarfComponent{
-						{Name: "component", Charts: []zarfTypes.ZarfChart{{Name: "chart"}}},
-					},
-				}},
+			bundlePackage: types.Package{
+				Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component": {"chart": {}}}},
+			zarfPackage: zarfTypes.ZarfPackage{
+				Components: []zarfTypes.ZarfComponent{
+					{Name: "component", Charts: []zarfTypes.ZarfChart{{Name: "chart"}}},
+				},
+			},
 			wantErr: false,
 		},
 		{
 			name:        "validOverrideMultipleComponents",
 			description: "Respective components and charts exist for override when multiple charts and components are present",
-			args: args{
-				bundlePackage: types.Package{
-					Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component-a": {"chart-1": {}}}},
-				zarfPackage: zarfTypes.ZarfPackage{
-					Components: []zarfTypes.ZarfComponent{
-						{Name: "component-a", Charts: []zarfTypes.ZarfChart{{Name: "chart-1"}, {Name: "chart-2"}}},
-						{Name: "component-b", Charts: []zarfTypes.ZarfChart{{Name: "chart-b"}}},
-					},
-				}},
+			bundlePackage: types.Package{
+				Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component-a": {"chart-1": {}}}},
+			zarfPackage: zarfTypes.ZarfPackage{
+				Components: []zarfTypes.ZarfComponent{
+					{Name: "component-a", Charts: []zarfTypes.ZarfChart{{Name: "chart-1"}, {Name: "chart-2"}}},
+					{Name: "component-b", Charts: []zarfTypes.ZarfChart{{Name: "chart-b"}}},
+				},
+			},
 			wantErr: false,
 		},
 		{
 			name:        "invalidComponentOverride",
 			description: "Component does not exist for override",
-			args: args{
-				bundlePackage: types.Package{
-					Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"hell-unleashed": {"chart": {}}}},
-				zarfPackage: zarfTypes.ZarfPackage{
-					Components: []zarfTypes.ZarfComponent{
-						{Name: "hello-world", Charts: []zarfTypes.ZarfChart{{Name: "chart"}}},
-					},
-				}},
+			bundlePackage: types.Package{
+				Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"hell-unleashed": {"chart": {}}}},
+			zarfPackage: zarfTypes.ZarfPackage{
+				Components: []zarfTypes.ZarfComponent{
+					{Name: "hello-world", Charts: []zarfTypes.ZarfChart{{Name: "chart"}}},
+				},
+			},
 			wantErr: true,
 		},
 		{
 			name:        "invalidChartOverride",
 			description: "Chart does not exist for override",
-			args: args{
-				bundlePackage: types.Package{
-					Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component": {"hell-unleashed": {}}}},
-				zarfPackage: zarfTypes.ZarfPackage{
-					Components: []zarfTypes.ZarfComponent{
-						{Name: "component", Charts: []zarfTypes.ZarfChart{{Name: "hello-world"}}},
-					},
-				}},
+			bundlePackage: types.Package{
+				Name: "foo", Overrides: map[string]map[string]types.BundleChartOverrides{"component": {"hell-unleashed": {}}}},
+			zarfPackage: zarfTypes.ZarfPackage{
+				Components: []zarfTypes.ZarfComponent{
+					{Name: "component", Charts: []zarfTypes.ZarfChart{{Name: "hello-world"}}},
+				},
+			},
 			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := validateOverrides(tt.args.bundlePackage, tt.args.zarfPackage)
+			err := validateOverrides(tt.bundlePackage, tt.zarfPackage)
 			if tt.wantErr {
 				require.Error(t, err)
 				return
@@ -158,65 +140,52 @@ func Test_validateOverrides(t *testing.T) {
 }
 
 func Test_getPkgPath(t *testing.T) {
-	type args struct {
+	tests := []struct {
+		name   string
 		pkg    types.Package
 		arch   string
 		srcDir string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
+		want   string
 	}{
 		{
-			name: "init full path",
-			args: args{
-				pkg:    types.Package{Name: "init", Ref: "0.0.1", Path: "../fake/path/custom-init.tar.zst"},
-				arch:   "fake64",
-				srcDir: "/mock/source",
-			},
-			want: "/mock/fake/path/custom-init.tar.zst",
+			name:   "init full path",
+			pkg:    types.Package{Name: "init", Ref: "0.0.1", Path: "../fake/path/custom-init.tar.zst"},
+			arch:   "fake64",
+			srcDir: "/mock/source",
+			want:   "/mock/fake/path/custom-init.tar.zst",
 		},
 		{
-			name: "init directory only path",
-			args: args{
-				pkg:    types.Package{Name: "init", Ref: "0.0.1", Path: "../fake/path"},
-				arch:   "fake64",
-				srcDir: "/mock/source",
-			},
-			want: "/mock/fake/path/zarf-init-fake64-0.0.1.tar.zst",
+			name:   "init directory only path",
+			pkg:    types.Package{Name: "init", Ref: "0.0.1", Path: "../fake/path"},
+			arch:   "fake64",
+			srcDir: "/mock/source",
+			want:   "/mock/fake/path/zarf-init-fake64-0.0.1.tar.zst",
 		},
 		{
-			name: "full path",
-			args: args{
-				pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "./fake/zarf-package-nginx-fake64-0.0.1.tar.zst"},
-				arch:   "fake64",
-				srcDir: "/mock/source",
-			},
-			want: "/mock/source/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
+			name:   "full path",
+			pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "./fake/zarf-package-nginx-fake64-0.0.1.tar.zst"},
+			arch:   "fake64",
+			srcDir: "/mock/source",
+			want:   "/mock/source/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
 		},
 		{
-			name: "directory only path",
-			args: args{
-				pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "fake"},
-				arch:   "fake64",
-				srcDir: "/mock/source",
-			},
-			want: "/mock/source/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
+			name:   "directory only path",
+			pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "fake"},
+			arch:   "fake64",
+			srcDir: "/mock/source",
+			want:   "/mock/source/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
 		},
 		{
-			name: "absolute path",
-			args: args{
-				pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "/fake/zarf-package-nginx-fake64-0.0.1.tar.zst"},
-				arch:   "fake64",
-				srcDir: "/mock/source",
-			},
-			want: "/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
+			name:   "absolute path",
+			pkg:    types.Package{Name: "nginx", Ref: "0.0.1", Path: "/fake/zarf-package-nginx-fake64-0.0.1.tar.zst"},
+			arch:   "fake64",
+			srcDir: "/mock/source",
+			want:   "/fake/zarf-package-nginx-fake64-0.0.1.tar.zst",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			path := getPkgPath(tt.args.pkg, tt.args.arch, tt.args.srcDir)
+			path := getPkgPath(tt.pkg, tt.arch, tt.srcDir)
 			require.Equal(t, tt.want, path)
 		})
 	}
