7dd6d549d8724ff40410600d493c917e9336ffd9
Tristan Holaday <40547442+TristanHoladay@users.noreply.github.com>
2024-08-18 14:25:00 -0600

    chore: update tests to use runCmd() (#823)
    Co-authored-by: UncleGedd <42304551+UncleGedd@users.noreply.github.com>

--- src/test/e2e/monitor_test.go
+++ src/test/e2e/monitor_test.go
@@ -5,7 +5,6 @@
 package test
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -14,27 +13,26 @@ import (
 func TestMonitor(t *testing.T) {
 	// this test assumes a running k3d cluster with the UDS operator and admission controller installed
 	// recommend running with uds run test:engine-e2e to install controllers
-	cmd := strings.Split("zarf tools kubectl get deployments -n pepr-system -o=jsonpath='{.items[*].metadata.name}'", " ")
-	deployments, _, _ := e2e.UDS(cmd...)
+	deployments, _ := runCmd(t, "zarf tools kubectl get deployments -n pepr-system -o=jsonpath='{.items[*].metadata.name}'")
 	require.Contains(t, deployments, "pepr-uds-core")
 	require.Contains(t, deployments, "pepr-uds-core-watcher")
 
 	// we expect this command to fail because UDS Core doesn't allow some of the configs in this package
-	_, _, err := e2e.UDS("zarf", "dev", "deploy", "src/test/packages/engine", "--retries=1")
+	_, _, err := runCmdWithErr("zarf dev deploy src/test/packages/engine --retries=1")
 	require.Error(t, err)
 
 	t.Run("test mutated policies", func(t *testing.T) {
-		stdout, _, _ := e2e.UDS("monitor", "pepr", "mutated")
+		stdout, _ := runCmd(t, "monitor pepr mutated")
 		require.Contains(t, stdout, "✎ MUTATED   podinfo")
 	})
 
 	t.Run("test allowed policies", func(t *testing.T) {
-		stdout, _, _ := e2e.UDS("monitor", "pepr", "allowed")
+		stdout, _ := runCmd(t, "monitor pepr allowed")
 		require.Contains(t, stdout, "✓ ALLOWED   podinfo")
 	})
 
 	t.Run("test denied policies", func(t *testing.T) {
-		stdout, _, _ := e2e.UDS("monitor", "pepr", "denied")
+		stdout, _ := runCmd(t, "monitor pepr denied")
 		require.Contains(t, stdout, "✗ DENIED    podinfo")
 	})
 }
