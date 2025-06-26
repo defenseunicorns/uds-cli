21522b1278f7500f3040da79eabf8280278f5266
Erickson Moskito <erickson@defenseunicorns.com>
2025-06-26 12:42:24 -0700

    chore: Add local PackageSource interface with NewLoadOptionsForRemove method signature to replace Zarf PackageSource removed in v0.56.0

--- src/pkg/sources/new.go
+++ src/pkg/sources/new.go
@@ -13,14 +13,39 @@ import (
 	"github.com/defenseunicorns/uds-cli/src/config"
 	"github.com/defenseunicorns/uds-cli/src/types"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
-	zarfSources "github.com/zarf-dev/zarf/src/pkg/packager/sources"
+	"github.com/zarf-dev/zarf/src/pkg/packager"
 	"github.com/zarf-dev/zarf/src/pkg/zoci"
 	zarfTypes "github.com/zarf-dev/zarf/src/types"
 )
 
+// PackageSource is an interface for package sources lifted from Zarf v0.55.6.
+//
+// While this interface defines three functions, LoadPackage, LoadPackageMetadata, and Collect; only one of them should be used within a packager function.
+//
+// These functions currently do not promise repeatability due to the side effect nature of loading a package.
+//
+// Signature and integrity validation is up to the implementation of the package source.
+//
+//	`sources.ValidatePackageSignature` and `sources.ValidatePackageIntegrity` can be leveraged for this purpose.
+type PackageSource interface {
+	// NewLoadOptionsForRemove creates a new load options for remove.
+	NewLoadOptionsForRemove() packager.LoadOptions
+
+	/* TODO(remove): For Reference Only
+	// LoadPackage loads a package from a source.
+	LoadPackage(ctx context.Context, dst *layout.PackagePaths, filter filters.ComponentFilterStrategy, unarchiveAll bool) (pkg v1alpha1.ZarfPackage, warnings []string, err error)
+
+	// LoadPackageMetadata loads a package's metadata from a source.
+	LoadPackageMetadata(ctx context.Context, dst *layout.PackagePaths, wantSBOM bool, skipValidation bool) (pkg v1alpha1.ZarfPackage, warnings []string, err error)
+
+	// Collect relocates a package from its source to a tarball in a given destination directory.
+	Collect(ctx context.Context, destinationDirectory string) (tarball string, err error)
+	*/
+}
+
 // NewFromLocation creates a new package source based on pkgLocation
-func NewFromLocation(bundleCfg types.BundleConfig, pkg types.Package, opts zarfTypes.ZarfPackageOptions, sha string, nsOverrides NamespaceOverrideMap) (zarfSources.PackageSource, error) {
-	var source zarfSources.PackageSource
+func NewFromLocation(bundleCfg types.BundleConfig, pkg types.Package, opts zarfTypes.ZarfPackageOptions, sha string, nsOverrides NamespaceOverrideMap) (PackageSource, error) {
+	var source PackageSource
 	var pkgLocation string
 	if bundleCfg.DeployOpts.Source != "" {
 		pkgLocation = bundleCfg.DeployOpts.Source
