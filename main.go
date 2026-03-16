920969b4d5bd3fc280219ccc332f29fe9a3a2836
Austin Abro <austinabro321@gmail.com>
2026-03-16 15:51:03 -0400

    better verbiage
    Signed-off-by: Austin Abro <austinabro321@gmail.com>

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	// Decision point here for the uds-cli team, with ssa deploys what do you want your field manager to be named
 	// You could name it "zarf" to be consistent with the name set by Zarf, this will allow users to deploy the same resources with packages and bundles interchangeably
 	// Alternatively, you could name it uds, if you want to introduce friction if a user deploys a package previously owned by uds-cli with Zarf or vice versa
-	// kube.ManagedFieldsManager = "uds|zarf"
+	// kube.ManagedFieldsManager = "uds|zarf" (there is a constant cluster.FieldManagerName if using zarf)
 	cmd.Execute()
 }
