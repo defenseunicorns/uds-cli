0430ac06162eb8ea7ef143d1ee78a2a03f36341e
UnicornChance <chance@defenseunicorns.com>
2026-03-16 21:43:33 -0600

    chore(docs): create cli docs and remove old docs and their generation

--- hack/generate-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# Copyright 2024 Defense Unicorns
-# SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
-
-
-# Create the CLI docs for UDS CLI
-# set UDS_NO_PROGRESS to empty string to override uds run default behavior
-UDS_NO_PROGRESS="" go run main.go internal gen-cli-docs
--- src/cmd/internal.go
+++ src/cmd/internal.go
@@ -8,17 +8,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/alecthomas/jsonschema"
 	runnerCLI "github.com/defenseunicorns/maru-runner/src/cmd"
 	"github.com/defenseunicorns/uds-cli/src/config/lang"
-	"github.com/defenseunicorns/uds-cli/src/pkg/message"
 	"github.com/defenseunicorns/uds-cli/src/types"
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
 )
 
 var internalCmd = &cobra.Command{
@@ -53,66 +48,9 @@ var configTasksSchemaCmd = &cobra.Command{
 	},
 }
 
-var genCLIDocs = &cobra.Command{
-	Use:   "gen-cli-docs",
-	Short: lang.CmdInternalGenerateCliDocsShort,
-	RunE: func(_ *cobra.Command, _ []string) error {
-		// Don't include the datestamp in the output
-		rootCmd.DisableAutoGenTag = true
-
-		rootCmd.RemoveCommand(zarfCli)
-
-		// Set the default value for the uds-cache flag (otherwise this defaults to the user's home directory)
-		rootCmd.Flag("uds-cache").DefValue = "~/.uds-cache"
-
-		// remove existing docs but ignore the _index.md
-		glob, err := filepath.Glob("./docs/reference/CLI/commands/uds*.md")
-		if err != nil {
-			return err
-		}
-		for _, f := range glob {
-			err := os.Remove(f)
-			if err != nil {
-				return err
-			}
-		}
-
-		var prependTitle = func(s string) string {
-			fmt.Println(s)
-			name := filepath.Base(s)
-
-			// strip .md extension
-			name = name[:len(name)-3]
-
-			// replace _ with space
-			title := strings.Replace(name, "_", " ", -1)
-
-			return fmt.Sprintf(`---
-title: %s
-description: UDS CLI command reference for <code>%s</code>.
----
-`, title, title)
-		}
-
-		var linkHandler = func(link string) string {
-			return "/reference/cli/commands/" + link[:len(link)-3] + "/"
-		}
-
-		// glob, err := filepath.Glob("./docs/reference/CLI/commands/uds*.md")
-		err = doc.GenMarkdownTreeCustom(rootCmd, "./docs/reference/CLI/commands/", prependTitle, linkHandler)
-		if err != nil {
-			return err
-		}
-
-		message.Success(lang.CmdInternalGenerateCliDocsSuccess)
-		return nil
-	},
-}
-
 func init() {
 	rootCmd.AddCommand(internalCmd)
 
-	internalCmd.AddCommand(genCLIDocs)
 	internalCmd.AddCommand(configUDSSchemaCmd)
 	internalCmd.AddCommand(configTasksSchemaCmd)
 }
